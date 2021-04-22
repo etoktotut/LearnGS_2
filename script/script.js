c56bbfc666416267391c43d5896ade4c43bc25bb
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-22 22:21:35 +0300

    HT19 regular

--- script/script.js
+++ script/script.js
@@ -51,4 +51,81 @@ window.addEventListener('DOMContentLoaded', () => {
         myInterval = setInterval(updateClock, 1000);
     }
     countTimer('1 july 2021');
+
+    //меню
+
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+
+
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+        menuItems.forEach(item => item.addEventListener('click', handlerMenu));
+
+    };
+
+    toggleMenu();
+
+    //popup
+
+    const popupAnimationY = (popup, time) => {
+        const start = Date.now(); // запомнить время начала
+        popup.style.transform = 'translateY(-100%)';
+        popup.style.display = 'block';
+        // шаг смещения за одну 20ms
+        const step = Math.ceil((document.documentElement.clientHeight / time) * 20);
+        // положение по оси Y
+        let top = 0;
+
+        function draw(top) {
+            popup.style.top = top + 'px';
+        }
+
+
+        const timer = setInterval(() => {
+            // сколько времени прошло с начала анимации?
+            const timePassed = Date.now() - start;
+            top += step;
+            draw(top);
+            //если время вышло или окно целиком на странице - то прекращаем работать
+            if (timePassed >= time || top >= document.documentElement.clientHeight) {
+                clearInterval(timer);
+                return;
+            }
+
+        }, 20); //20ms = 50fps
+
+    };
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+        popupBtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                //если экран устройства уже 768px
+                if (screen.width <= 768) {
+                    popup.style.display = 'block';
+                } else {
+                    popupAnimationY(popup, 200);
+                }
+            });
+        });
+
+        popUpClose.addEventListener('click', () => popup.style.display = 'none');
+
+    };
+
+    togglePopUp();
+
 });
