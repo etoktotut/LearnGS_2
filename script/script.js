43f39a4530148cc353456042d0a64f0441575556
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-25 10:29:31 +0300

    bulk update of scroll from 20 and 19

--- script/script.js
+++ script/script.js
@@ -54,29 +54,84 @@ window.addEventListener('DOMContentLoaded', () => {
 
     //меню
 
+    const smoothScroll = (targetYposition, timeOfScroll) => {
+        const startTime = Date.now();
+        let animFrameId;
+        let step = (targetYposition / timeOfScroll) * 16; //расчет шага смещения (16ms - 60fps)
+        const moveUp = step < 0; //направление движения, если шаг смещения отрицательный - двигаем страницу Up
+        let leftToEnd = Math.abs(targetYposition); // остаток перемещения - постоянно корректируется
+        let currentYposition = window.pageYOffset; //стартовая точка
+        targetYposition += window.pageYOffset; //цель (точка прибытия)
+
+        const drawDown = () => {
+            const timePassed = Date.now() - startTime;
+            //точно вписываемся на последнем шаге (последний шаг может быть как положительным, так и отрицательным)
+            step = leftToEnd < Math.abs(step) ? step > 0 ? leftToEnd : -leftToEnd : step;
+            currentYposition += step;
+            window.scrollTo(0, currentYposition);
+            leftToEnd -= Math.abs(step); // корректируем оставшееся расстояние после перемещения окна
+            //если время вышло или окно в нужном месте на странице - то прекращаем анимацию
+            if (timePassed >= timeOfScroll || (moveUp ? currentYposition <= targetYposition : currentYposition >= targetYposition)) {
+                window.scrollTo(0, targetYposition);
+                cancelAnimationFrame(animFrameId);
+                return;
+            }
+            animFrameId = requestAnimationFrame(drawDown);
+        };
+        drawDown();
+    };
+
     const toggleMenu = () => {
 
         const menu = document.querySelector('menu'),
             wbody = document.querySelector('body');
 
-        const handlerMenu = () => {
+        const handlerMenu = event => {
+            event.preventDefault();
             menu.classList.toggle('active-menu');
         };
 
+        const scrollMenu = event => {
+            handlerMenu(event);
+            const blockID = event.target.closest('li').querySelector('a').getAttribute('href');
+            const blockY = document.querySelector(`${blockID}`).getBoundingClientRect().y;
+            smoothScroll(blockY, 500);
+        };
+
+
         wbody.addEventListener('click', event => {
             const target = event.target;
+            const isLi = target.closest('li');
+
             if (menu.classList.contains('active-menu')) {
-                if (!target.closest('menu') || target.classList.contains('close-btn') || (target.tagName === 'A')) {
-                    handlerMenu();
+                if (isLi) {
+                    scrollMenu(event);
+                } else if (!target.closest('menu') || target.classList.contains('close-btn')) {
+                    handlerMenu(event);
                 }
             } else if (target.closest('.menu')) {
-                handlerMenu();
+                handlerMenu(event);
             }
         });
     };
 
     toggleMenu();
 
+    //кнопка внизу первой страницы страницы
+    const imgDownScroll = () => {
+        const imgDown = document.querySelector('img[src = "images/scroll.svg"]');
+        imgDown.addEventListener('click', () => {
+            const blckId = imgDown.parentNode.getAttribute('href');
+            const blckY = document.querySelector(`${blckId}`).getBoundingClientRect().y;
+            smoothScroll(blckY, 500);
+        });
+    };
+
+    imgDownScroll();
+
+
+
+    //popup
     const animate = (elem, time) => {
         const startTime = Date.now(); // запомнить время начала
         elem.style.top = '-100%'; // двигаем меню за пределы экрана
