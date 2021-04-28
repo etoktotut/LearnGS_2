fbc68e7bbbcf2b181249a8f93cf87761b29ec147
Dmitry Kevdin <vdin2001@mail.ru>
2021-04-28 15:52:35 +0300

    ht23 _1

--- script/script.js
+++ script/script.js
@@ -355,4 +355,83 @@ window.addEventListener('DOMContentLoaded', () => {
 
     slider();
 
+    // command
+
+    const ourCommand = () => {
+        const imgCommandElems = document.querySelectorAll('.command .command__photo');
+
+        const imgExchange = e => {
+            let temp = e.target.src;
+            e.target.src = e.target.dataset.img;
+            e.target.dataset.img = temp;
+        };
+
+        imgCommandElems.forEach(item => item.addEventListener('mouseenter', imgExchange));
+        imgCommandElems.forEach(item => item.addEventListener('mouseleave', imgExchange));
+    };
+
+    ourCommand();
+
+    //validations
+
+
+    //только Кириллица + дефис и пробел
+    const withotEngSymbols = elem => elem.addEventListener('input', e => elem.value = elem.value.replace(/[^а-яёА-ЯЁ\-\ ]/gi, ''));
+
+    //только цифры
+    const approvedDigits = elem => elem.addEventListener('input', e => elem.value = elem.value.replace(/\D/g, ''));
+    //e-mail 
+    const approvedEmail = elem => elem.addEventListener('input', e => elem.value = elem.value.replace(/[^a-zA-Z@\-_!~\'\.\*]/gi, ''));
+
+    //phone
+    const approvedPhone = elem => elem.addEventListener('input', e => elem.value = elem.value.replace(/[^\d-\(\)]/g, ''));
+
+    const calcValidations = () => {
+        const calcInputs = document.querySelectorAll('#calc input');
+        calcInputs.forEach(approvedDigits);
+    };
+    calcValidations();
+
+    const textValidations = () => {
+        const userMessage = document.querySelector('input[name="user_message"]');
+        const userNames = document.querySelectorAll('input[name="user_name"]');
+        userNames.forEach(withotEngSymbols);
+        withotEngSymbols(userMessage);
+    };
+    textValidations();
+
+    const emailValidations = () => {
+        const userEmails = document.querySelectorAll('input[name="user_email"]');
+        userEmails.forEach(approvedEmail);
+
+    };
+    emailValidations();
+
+    const phoneValidations = () => {
+        const userPhones = document.querySelectorAll('input[name="user_phone"]');
+        userPhones.forEach(approvedPhone);
+
+    };
+    phoneValidations();
+
+    const valueValidation = elem => elem.addEventListener('blur', e => {
+        elem.value = elem.value.replace(/^[ -]*/, '');
+        elem.value = elem.value.replace(/[ -]*$/, '');
+        elem.value = elem.value.replace(/ +/g, ' ');
+        elem.value = elem.value.replace(/-+/g, '-');
+
+        if (elem.name === 'user_name') {
+            elem.value = elem.value.replace(/[а-яА-Я]+/g, (match) => match.slice(0, 1).toUpperCase() + match.slice(1).toLowerCase());
+        }
+
+    });
+
+    const blurValidation = () => {
+        const allInputs = document.querySelectorAll('input');
+        allInputs.forEach(valueValidation);
+
+    };
+
+    blurValidation();
+
 });
